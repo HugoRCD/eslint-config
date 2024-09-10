45c9dd7734d453ac6b0ba5a7082fb2c01a187ecd
HugoRCD <hugo.richard@epitech.eu>
2024-09-10 13:55:37 +0200

    Test ?

--- apps/nuxt-app/components/Noise.vue
+++ apps/nuxt-app/components/Noise.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pointer-events-none  inset-0 fixed z-10 size-full overflow-hidden">
+  <div class="pointer-events-none    inset-0 fixed z-10 size-full overflow-hidden">
     <div class="noise pointer-events-none absolute inset-[-200%] z-50 size-[400%] bg-[url('/noise.png')] opacity-[6%]" />
   </div>
 </template>
