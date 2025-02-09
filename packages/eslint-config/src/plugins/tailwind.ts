2003d95ed49621752204f71ea7127f7d99a604ab
HugoRCD <hugo.richard@epitech.eu>
2025-02-09 17:34:54 +0100

    up

---
+++ packages/eslint-config/src/plugins/tailwind.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import pluginTailwindcss from 'eslint-plugin-tailwindcss'
+import type { Linter } from 'eslint'
+
+export default function tailwindcss(): Linter.Config[] {
+  return [
+    {
+      name: 'tailwindcss/rules',
+      plugins: {
+        tailwindcss: pluginTailwindcss
+      },
+      rules: {
+        'tailwindcss/classnames-order': 'error',
+        'tailwindcss/no-contradicting-classname': 'error',
+        'tailwindcss/enforces-shorthand': 'error',
+        'tailwindcss/no-custom-classname': 'off',
+        'tailwindcss/no-unnecessary-arbitrary-value': 'off',
+      }
+    }
+  ]
+}
