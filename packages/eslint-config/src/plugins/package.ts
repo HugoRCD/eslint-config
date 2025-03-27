83ae0c55e83a87faf619d808bbabcc70e9cb8ce1
HugoRCD <hugo.richard@epitech.eu>
2025-03-27 20:57:27 +0100

    feat: add continuous release

--- packages/eslint-config/src/plugins/package.ts
+++ packages/eslint-config/src/plugins/package.ts
@@ -1,18 +1,19 @@
 import type { Linter } from 'eslint'
-import packageJson from 'eslint-plugin-package-json/configs/recommended'
+import packageJson from 'eslint-plugin-package-json'
 import { PackageJsonConfigOptions } from '../types'
 
 export default function packageJsonPlugin(options?: PackageJsonConfigOptions): Linter.Config[] {
   return [
     {
-      ...packageJson,
+      ...packageJson.configs.recommended,
       rules: {
         'package-json/order-properties': options?.strict ? 'error' : 'off',
         'package-json/repository-shorthand': 'error',
         'package-json/sort-collections': options?.strict ? 'error' : 'off',
+        'package-json/require-author': options?.strict ? 'error' : 'off',
         'package-json/unique-dependencies': 'error',
         'package-json/valid-package-def': 'error',
-        'package-json/valid-version': 'error',
+        'package-json/valid-version': 'off',
       },
     } as Linter.Config,
   ]
